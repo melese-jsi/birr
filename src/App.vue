bf8139ff4b329d3f4b86d6869df3e76e476592fa
Melese Tamiru <mtamiru@psiet.org>
2024-08-30 11:58:50 +0300

    added values

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,7 @@
        align-tabs="center"
       color="deep-purple-accent-4"
       stacked
+      text-wrap
     >
       <v-tab value="usd">
         <v-icon icon="mdi-currency-usd"></v-icon>
@@ -21,7 +22,27 @@
       <v-tab value="gbp">
         <v-icon icon="mdi-currency-gbp"></v-icon>
         GBP</v-tab>
+        <v-menu><template v-slot:activator="{ props }">
+              <v-btn
+                class="align-self-center me-4"
+                height="100%"
+                rounded="0"
+                variant="plain"
+                v-bind="props"
+              >
+                <span class="font-weight-bold">more</span>
+
+                <v-icon icon="mdi-menu-down" end></v-icon>
+              </v-btn>
+            </template>
+            <v-list class="bg-grey-lighten-3" min-width="20px">
+              <v-list-item @click="addItem('boa')">BOA</v-list-item>
+              <v-list-item @click="addItem('cbe')">CBE</v-list-item>
+            </v-list>
+
+          </v-menu>
     </v-tabs>
+
 
     <v-card-text>
       <v-tabs-window v-model="tab">
@@ -36,6 +57,13 @@
         <v-tabs-window-item value="gbp">
           <gbpcardView></gbpcardView>
         </v-tabs-window-item>
+        <v-tabs-window-item value="boa">
+          <boacardView></boacardView>  
+        </v-tabs-window-item>
+        <v-tabs-window-item value="cbe">
+          <eurcardView></eurcardView>
+        </v-tabs-window-item>
+
       </v-tabs-window>
     </v-card-text>
   </v-card>
@@ -50,16 +78,24 @@
 import usdcardView from  '@/components/usdcard.vue'
 import eurcardView from '@/components/eurcard.vue'
 import gbpcardView from '@/components/gbpcard.vue'
+import boacardView from '@/components/boacard.vue'
 export default {
   name: "App",
   components:{
     usdcardView,
     eurcardView,
-    gbpcardView
+    gbpcardView,
+    boacardView
   },
 
   data: () => ({
-    tab:null
+    tab:'usd'
   }),
+  methods: {
+      addItem (item) {
+        
+        this.$nextTick(() => { this.tab = item })
+      },
+    }
 };
 </script>
---
+++ src/components/boacard.vue
@@ -0,0 +1,105 @@
+<template>
+    <v-app>
+      
+        <v-card
+    class="mx-auto"
+    prepend-icon="mdi-currency-gbp"
+     
+   
+  >
+    <template v-slot:title>
+      <span class="font-weight-black text-wrap">Recent USD exchange rates of BOA</span>
+      
+    </template>
+
+    <v-card-text class="bg-surface-light pt-4 text-wrap">
+       
+    </v-card-text>
+    <v-timeline align="start" density="compact">
+        <v-timeline-item
+          v-for="message in thismonth"
+          :key="message.date"
+          :dot-color="red"
+          :icon="message.icon"
+          size="xsmall"
+          hide-dot=true
+        >
+        
+          <div class="mb-4">
+            <strong class="pa-2">{{ message.date }}</strong>
+           
+            <v-divider></v-divider>
+            
+            <div><span class="text-primary pa-3">Buying</span> &mdash; <span class="chip">{{ message.buying }} </span>
+              <br/>
+              <span class="text-primary pa-3">Selling </span>&mdash;<span class="chip1" > {{ message.selling }}</span>
+            </div>
+          </div>
+        </v-timeline-item>
+      </v-timeline>
+    </v-card>
+        
+    </v-app>
+</template>
+
+<script>
+const httpdata = async ()=>{
+    console.log("heyyy")
+    
+    const response = await fetch("https://banksethiopia.com/wp-json/graph/v1/all?bankName=abyssinia&dateRange=ThisMonth")
+   
+    
+    const data_thismonth = await response.json()
+    const response2 = await fetch("https://banksethiopia.com/wp-json/graph/v1/all?bankName=abyssinia&dateRange=LastMonth")
+    const data_lastmonth = await response2.json()
+
+    //format data for presentation
+    let thismonth = data_thismonth[0]
+    let usd_data =[]
+    if (thismonth)
+    {
+        let thismonth_usd_buying = thismonth['USD']['buying']
+        let thismonth_usd_selling = thismonth['USD']['selling']
+        let thismonth_dates = thismonth['categories']
+
+        
+        for(let i=0;i<=thismonth_dates.length; i++)
+        {
+            //reverse them
+            thismonth_usd_buying.reverse()
+            thismonth_usd_selling.reverse()
+            thismonth_dates.reverse()
+            let temp ={}
+            temp['date']=thismonth_dates[i]
+            temp['buying']=thismonth_usd_buying[i]
+            temp['selling']=thismonth_usd_selling[i]
+            usd_data.push(temp)
+        }
+
+    }
+     
+    console.log(usd_data)
+
+  return usd_data
+}
+
+export default {
+    name: "boacardView",
+    data() {
+    return { thismonth: [] ,
+        lastmonth:[]
+    };
+  },
+  mounted() {
+    httpdata()
+      .then((result1) => {
+        this.thismonth = result1;
+        //this.lastmonth = result2;
+      })
+      .catch((err) => console.log("error " + err.message));
+  }
+}
+</script>
+
+        
+      
