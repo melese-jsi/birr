c3a73ea36863456ab74a52437f7bb18203f344c8
Melese <melese@Meleses-MacBook-Pro.local>
2024-09-12 08:14:04 +0300

    font size

--- src/components/card2.vue
+++ src/components/card2.vue
@@ -14,20 +14,20 @@
     <v-list-item v-for="item in data" :key="item.title">
  <v-card variant="outlined" class="mt-3 mb-3 rounded-lg" elevation="24" density="compact" rounded="24" >
   <v-card-title v-if="history==null">{{ getBankName(item.title) }}</v-card-title>
-  <v-card-title v-if="history"><span class="text-subtitle">{{ item.title }}</span></v-card-title>
+  <v-card-title v-if="history" class="text-subtitle"><span class="text-subtitle">{{ item.title }}</span></v-card-title>
   <!-- <v-card-subtitle>{{ new Date().toDateString() }}</v-card-subtitle> -->
   <v-divider class="mt-1 mb-2"></v-divider>
   <v-card-text class="py-0">
         <v-row >
           <v-col
-            class="text-h5" style="color:#1C9BC1"
+            class="text-subtitle font-weight-medium" style="color:#1C9BC1"
             cols="6"
 
           >
            {{ parseFloat(item.buying).toFixed(2) }}
           </v-col>
   
-          <v-col class=" text-h6" style="color:#BDD52D" cols="6">
+          <v-col class=" text-subtitle font-weight-medium" style="color:#BDD52D" cols="6">
             {{parseFloat(item.selling).toFixed(2)}}
           </v-col>
         </v-row>
