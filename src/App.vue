3c059f459f8140b9f6245ff3a87e2c916e894049
melese-jsi <36546008+melese-jsi@users.noreply.github.com>
2024-12-17 15:46:32 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -199,8 +199,8 @@ const formatted_data = (data,currency)=>{
      if (obj !=undefined){
       temp.title=obj['bank'].charAt(0).toUpperCase() + obj['bank'].slice(1)
        console.log(obj['buying'],obj['selling'])
-      temp.buying= obj['buying'] ? obj['buying'].toFixed(2):""
-      temp.selling=obj['selling'] ? obj['selling'].toFixed(2):""
+      temp.buying= obj['buying'] !== null ? obj['buying'].toFixed(2):""
+      temp.selling=obj['selling'] !== null ? obj['selling'].toFixed(2):""
       temp.subtitle = `<span class="text-primary pa-3">Buying</span> &mdash; <span class="chip"> `+obj['buying'].toFixed(2)+` </span><br/><br/> <span class="text-primary pa-3">Selling </span>&mdash;<span class="chip1"> `+obj['selling'].toFixed(2)+`</span>`
       //temp.prependAvatar= logos[obj['bank']]
       temp.icon ="<v-icon='home'></v-icon>"
